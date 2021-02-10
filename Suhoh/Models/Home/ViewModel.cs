c9dac33eded6dab616aeb6805c08f911f1abc8c0
suhoh <suhohconsultingltd@gmail.com>
2021-02-09 21:31:39 -0700

    Added OpenLayerMap and ViewModel

---
+++ Suhoh/Models/Home/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Suhoh.Model
+{
+    public class ViewModel
+    {
+        public string MyId { get; set; }
+
+        public ViewModel()
+        {
+            MyId = "Hello world";
+        }
+    }
+}
