2da2c4e51f483d43a7132341a4278e43fac48bb5
Jysuh <Jysuh@DESKTOP-88TITA4>
2021-10-04 18:59:09 -0600

    Renamed main.js

--- Suhoh/App_Start/BundleConfig.cs
+++ Suhoh/App_Start/BundleConfig.cs
@@ -13,15 +13,15 @@ namespace Suhoh
                 "~/Scripts/BaseScript.js",
                 "~/Scripts/LeftPanelScript.js",
                 "~/Scripts/Ol_v650/ol.js",
-                "~/Scripts/OL/main.js",
+                "~/Scripts/OL/olMain.js",
                 "~/Scripts/OL/layersNsymbols.js",
                 "~/Scripts/OL/addLayers.js",
                 "~/Scripts/D3_v650/d3.js",
-                "~/Scripts/D3/main.js",
+                "~/Scripts/D3/d3Main.js",
                 "~/Scripts/D3/pie.js",
                 "~/Scripts/D3/bar.js",
                 "~/Scripts/D3/line.js",
-                "~/Scripts/Gridview/main.js",
+                "~/Scripts/Gridview/gridviewMain.js",
                 "~/Scripts/SheetJS/xlsx.full.min.js"
             ));
 
